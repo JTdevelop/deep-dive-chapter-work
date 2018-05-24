e426c793b7af1c07b9bd537ab57916be617570c5
Jay T <torrcorp@gmail.com>
2018-05-23 22:10:22 -0600

    Initial commit

---
+++ src/Guess.java
@@ -0,0 +1,29 @@
+class Guess {
+
+  public static void main(String args[])
+      throws java.io.IOException {
+
+    char ch, ignore, answer = 'D';
+    do {
+      System.out.println("I'm thinking of a letter between A and Z.");
+      System.out.print("Can you guess it:");
+
+      ch = (char) System.in.read(); // read a char from the keyboard
+
+      do {
+        ignore = (char) System.in.read();
+      }
+      while (ignore != '\n');
+
+      if (ch == answer)
+        System.out.println("** Right **");
+      else {
+      System.out.println("...Sorry, you're");
+      if (ch < answer)  System.out.println("too low");
+       else System.out.println("too high");
+       System.out.println("Try again!\n");
+      }
+    } while(answer !=ch);
+  }
+}
+
